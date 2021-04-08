95a9d916c267b8d6da57b2d27cce11fcc937905c
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-04-08 17:30:52 -0500

    A string is successfully reversed

--- reverse-phrase/src/app/app.component.ts
+++ reverse-phrase/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'reverse-phrase';
+    public title = "reverse-phrase";
+    public text = "";
+    public reverseText = "";
+
+    onClickReverse() {
+        //Event Listeners and Bindings Syntax
+        this.reverseText = this.text.split("").reverse().join("");
+    }
+
+    onChangeText(value: string) {
+        this.text = value;
+    }
+
+    onInputText(value: string) {
+        this.text = value;
+    }
 }
