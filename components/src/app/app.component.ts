a467fb00a1a1730479b2114f254e84ea9d7491c0
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-04-14 13:27:00 -0500

    Initialized the post componets

--- components/src/app/app.component.ts
+++ components/src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'components';
+
+  posts = [
+    {
+      img: 'https://picsum.photos/100',
+      author: 'John',
+      title: 'Title #1',
+    },
+    {
+      img: 'https://picsum.photos/100',
+      author: 'Mike',
+      title: 'Title #2',
+    },
+    {
+      img: 'https://picsum.photos/100',
+      author: 'Jane',
+      title: 'Title #3',
+    },
+  ];
 }
