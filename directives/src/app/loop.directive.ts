4fbedf1b4a4226010abe48c17faa778e07e6bd30
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-05-03 15:14:45 -0500

    Corrected the HTML formating

--- directives/src/app/loop.directive.ts
+++ directives/src/app/loop.directive.ts
@@ -1,10 +1,25 @@
-import { Directive } from '@angular/core';
+import { Directive, ViewContainerRef, TemplateRef, Input } from '@angular/core';
 
 @Directive({
-  selector: '[appLoop]'
+  selector: '[appLoop]',
 })
 export class LoopDirective {
+  constructor(
+    private viewContainer: ViewContainerRef, // Exposes methods for removing and inserting elements
+    private templateRef: TemplateRef<any> // provides a reference to which the children area attached
+  ) {}
 
-  constructor() { }
+  // Checks for a property called [appLoop]
+  @Input('appLoop') set render(steps: number) {
+    // Steps para
+    this.viewContainer.clear(); // Creates a clean slate before content insert
 
+    for (let step = 0; step < steps; step++) {
+      // Runs until the step var exceeds the steps para
+      this.viewContainer.createEmbeddedView(this.templateRef, {
+        // Inserting a new element
+        index: step,
+      });
+    }
+  }
 }
