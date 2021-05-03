776adf4e9f65803e0762d2f3798590bfbcc837cb
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-05-03 13:53:27 -0500

    Created a custom directive that mimics the ngClass directive

--- directives/src/app/class.directive.ts
+++ directives/src/app/class.directive.ts
@@ -1,11 +1,27 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appClass]',
 })
 export class ClassDirective {
-  constructor(private element: ElementRef) {
-    console.log('Class Directive ACTIVATED!!!');
-    this.element.nativeElement.style.color = 'red';
+  // Doesn't work because it is created before it can be bound.
+  // @Input('appClass') classnames: string;
+
+  constructor(private el: ElementRef) {}
+
+  /* @Input('appClass') set classnames(className: string) {
+    // Binds the property to the directive
+    this.el.nativeElement.classList.add(className);
+  } */
+
+  // Loops through the property obj and upon true will apply the class
+  @Input('appClass') set classnames(classObj: any) {
+    for (let key in classObj) {
+      if (classObj[key]) {
+        this.el.nativeElement.classList.add(key);
+      } else {
+        this.el.nativeElement.classList.remove(key);
+      }
+    }
   }
 }
