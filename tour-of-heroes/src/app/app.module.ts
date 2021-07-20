16657240412de88c5b437067d5b79290adcb1404
fmorrisey <forrestmorrisey@gmail.com>
2021-07-20 15:53:11 -0500

    Generated new comp

--- tour-of-heroes/src/app/app.module.ts
+++ tour-of-heroes/src/app/app.module.ts
@@ -8,12 +8,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
+import { MessagesComponent } from './components/messages/messages.component';
 
 // SERVICES
 import { HeroService } from './services/hero.service';
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+  ],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [HeroService],
   bootstrap: [AppComponent],
